f48932b507e69179632089e4a0fbcefe8c3f21b6
nelly.skinner <daniellaskinner120@gmail.com>
2021-01-11 21:56:47 +0000

    Prepared parsed language sample into desired data structure

--- language-app.py
+++ language-app.py
@@ -1,23 +1,42 @@
+import csv
 import nltk
 from nltk import ne_chunk, word_tokenize, sent_tokenize
 
+# is the language sample speech or text? y/n (if no ask for input for the text)
 # read from a text file a language sample
 text = input('Please enter a sentence or utterance to analyse. ')
 
 # tokenize the sample by sentence and word level
-sentence_tokens = sent_tokenize(text)
-print('Here is the sentence level tokenized language sample: {}'.format(sentence_tokens))
+sample_sentence_tokens = sent_tokenize(text)
+print('Here is the sentence level tokenized language sample: {}'.format(sample_sentence_tokens))
 
-word_tokens = word_tokenize(text)
-tagged_utterance = nltk.pos_tag(word_tokens)
-print('Here is the word level tokenized and parsed language sample: {}'.format(tagged_utterance))
-
-print('Here we can see chunking:')
-chunks = ne_chunk(tagged_utterance)
-print(chunks)
-print(chunks.__repr__())
+# function that takes sentence and word tokens and tags and chucks into data structure we want
+analysed_language_data = []
 
 
-# for each of the sentence_tokens in array display in a csv text file
+def insert_sentence_tokens_into_data_structure(sentence_tokens):
+    for sentence_token in sentence_tokens:
+        word_tokens = word_tokenize(sentence_token)
+        parsed_tokens = nltk.pos_tag(word_tokens)
 
-# display in a csv file for each of the word tokens in tagged_utterance with the tag code
+        current_sentence_dictionary = {
+            'sentence': sentence_token,
+            'parsed_tokens': []
+        }
+
+        for parsed_token in parsed_tokens:
+            current_parsed_token_dictionary = {
+                'token': parsed_token[0],
+                'tag': parsed_token[1]
+            }
+
+            current_sentence_dictionary['parsed_tokens'].append(current_parsed_token_dictionary)
+
+        current_sentence_dictionary['parsed_tokens'].pop()
+        analysed_language_data.append(current_sentence_dictionary)
+    return analysed_language_data
+
+
+# remove the full stops from the parsed tokens list of dictionaries
+parsed_language_data = insert_sentence_tokens_into_data_structure(sample_sentence_tokens)
+print(parsed_language_data)
